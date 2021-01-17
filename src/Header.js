feca379fa70cffc32dacab098e7f45209bbb47a6
mrlnlng <marlene.liang@hotmail.com>
2021-01-17 18:10:41 -0500

    adjusted responsive layout

--- src/Header.js
+++ src/Header.js
@@ -1,51 +1,71 @@
-import React from 'react'
-import { makeStyles } from '@material-ui/core/styles'; 
-import Logo from './images/logoStar.png'
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Logo from "./images/logoStar.png";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
 const useStyle = makeStyles({
-    title: {
-        fontFamily: "Montserrat",
-        fontWeight: "bold",
-        color: "#FFFFFF",
-        fontSize: "2.5rem",
-        marginBottom: "1rem",
-        marginTop: "3rem",
-        textAlign: "center"
-    },
-    subtitle: {
-        // width: "60vw",
-        color: "#FFFFFF",
-        fontWeight: "600",
-        textAlign: "center"
-    },
-    header: {
-        display: "flex",
+  title: {
+    fontFamily: "Montserrat",
+    fontWeight: "bold",
+    color: "#FFFFFF",
+    fontSize: "2.5rem",
+    textAlign: "center",
+  },
+  subtitle: props => ({
+    // width: "60vw",
+    color: "#FFFFFF",
+    fontWeight: "600",
+    textAlign: "center",
+    maxWidth: props.maxWidth
+  }),
+  header: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  titleContainer: (props) => ({
+    marginTop: "3rem",
+    display: "flex",
+    alignItems: props.alignItems,
+    flexDirection: props.flexDirection,
+    marginBottom: "1rem",
+  }),
+  image: props => ({
+    marginRight: "1.5rem",
+    marginBottom: props.marginBottom
+  }),
+});
+const Header = () => {
+  let props;
+  const phoneMatches = useMediaQuery("(max-width: 800x)");
+  if (phoneMatches) {
+    props = {
+      flexDirection: "row",
+      alignItems: "flex-end",
+      marginBottom: "0rem",
+      maxWidth: ""
+    };
+} else {
+    props = {
         flexDirection: "column",
         alignItems: "center",
-        justifyContent: "center"
-
-    },
-    titleContainer: {
-        display: "flex",
-        alignItems: "flex-end",
-        marginBottom: "1rem"
-    },
-    image: {
-        marginRight: "1.5rem"
-    }
-})
-const Header =()=> {
-    const classes = useStyle()
-    return(
-        <>
-        <div className={classes.header}>
-            <div className={classes.titleContainer}>
-            <img src={Logo} height="100" className={classes.image}/>
-    <div className={classes.title}>THE SHOPPIES</div>
-            </div>
-    <div className={classes.subtitle}>🎉 Nominate your 5 favourite movies for the Shoppies</div>
+        marginBottom: "0.5rem",
+        maxWidth: "90vw"
+    };
+  }
+  const classes = useStyle(props);
+  return (
+    <>
+      <div className={classes.header}>
+        <div className={classes.titleContainer}>
+          <img src={Logo} height="100" className={classes.image} />
+          <div className={classes.title}>THE SHOPPIES</div>
         </div>
-
+        <div className={classes.subtitle}>
+          🎉 Nominate your 5 favourite movies for the Shoppies
+        </div>
+      </div>
     </>
-    )
-}
+  );
+};
 export default Header;
